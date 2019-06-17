1b6c4b4d6e6ab29e52f9ef7f8269f296618e54d0
Daniel Kwong <dkwon014@ucr.edu>
2019-06-17 06:33:04 -0700

    initial commit

---
+++ product_lookup.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def findproduct( link, product ):
+    bro = product
+    url = link + '/products.json'
+    r = requests.get(url)
+    products = json.loads((r.text))['products']
+    for product in products:
+        productName = product['title']
+        searchA = productName.lower()
+        searchB = bro.lower()
+        if (searchA.find(searchB) != -1):
+            print(productName)
+            producturl = 'https://www.thefreshyard.com/products/' + product['handle']
+            print(producturl)
+bruv = input("Enter website url: ")
+yes = input("Enter product name: ")
+findproduct(bruv, yes)
